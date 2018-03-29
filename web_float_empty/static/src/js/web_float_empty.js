abe28ba74a585297c2913ab8f6ef9576da890268
Yasmine El Mrini <yasmine.elmrini@savoirfairelinux.com>
2018-03-29 18:36:01 -0400

    Fix widget

--- web_float_empty/static/src/js/web_float_empty.js
+++ web_float_empty/static/src/js/web_float_empty.js
@@ -5,7 +5,9 @@
 odoo.define('web_float_empty', function(require) {
     "use strict";
 
+    var core = require('web.core');
     var form_widgets = require('web.form_widgets');
+    var _t = core._t;
 
     form_widgets.FieldFloat.include({
         set_value: function(value_) {
@@ -15,8 +17,13 @@ odoo.define('web_float_empty', function(require) {
                 no_default_value set to 1.
             */
             if (this.options.no_default_value === 1){
+                var placeholder = '0';
                 var precision = this.digits[1];
-                this.node.attrs.placeholder = '0,' + '0'.repeat(precision);
+                if (precision != 0){
+                    placeholder += _t.database.parameters.decimal_point;
+                    placeholder += '0'.repeat(precision);
+                }
+                this.node.attrs.placeholder = placeholder;
                 this.set({'value': value_});
             } else {
                 this._super(value_);
